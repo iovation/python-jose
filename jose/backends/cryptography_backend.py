9d1014ae5fe2e12fe73e52d8f0265e30af2595e6
Adam Englander <adamenglander@yahoo.com>
2020-05-25 16:22:06 -0700

    Update cryptography backend to use OpenSSL RAND_bytes

--- jose/backends/cryptography_backend.py
+++ jose/backends/cryptography_backend.py
@@ -1,9 +1,9 @@
 from __future__ import division
 
 import math
-import os
 
 import six
+from cryptography.hazmat.bindings.openssl.binding import Binding
 
 try:
     from ecdsa import SigningKey as EcdsaSigningKey, \
@@ -31,10 +31,30 @@ from cryptography.hazmat.primitives.serialization import \
 from cryptography.utils import int_from_bytes, int_to_bytes
 from cryptography.x509 import load_pem_x509_certificate
 
+_binding = None
+
 
 def get_random_bytes(num_bytes):
-    # Change to use OpenSSL RAND_bytes
-    return bytes(os.urandom(num_bytes))
+    """
+    Get random bytes
+
+    Currently, Cryptography returns OS random bytes. If you want OpenSSL
+    generated random bytes, you'll have to switch the RAND engine after
+    initializing the OpenSSL backend
+    Args:
+        num_bytes (int): Number of random bytes to generate and return
+    Returns:
+        bytes: Random bytes
+    """
+    global _binding
+
+    if _binding is None:
+        _binding = Binding()
+
+    buf = _binding.ffi.new("char[]", num_bytes)
+    _binding.lib.RAND_bytes(buf, num_bytes)
+    rand_bytes = _binding.ffi.buffer(buf, num_bytes)[:]
+    return rand_bytes
 
 
 class CryptographyECKey(Key):
